80f0c62a661cb808d693a4a9453a212bdfd819fa
NguyenLeDuyHau-2280600889 <haudeptrai2108@gmail.com>
2026-02-02 15:04:50 +0700

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-/* GET home page. */
-//localhost:3000/api/v1/
-router.get('/', function (req, res, next) {
-  res.json({ message: 'Welcome to API v1', endpoints: ['/users', '/products', '/categories'] });
-});
-
-module.exports = router;
